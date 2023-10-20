59a17b9c42ddb7c90d0e45a7e99cc1b72b542de1
amurrai <andre.murrai@gmail.com>
2023-10-19 22:21:41 -0400

    Added assert function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🛑🛑🛑 Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("✅✅✅ Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
