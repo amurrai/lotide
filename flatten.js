d253076628a298c25446dfcb1ad7fbfe7cd3b1ab
amurrai <andre.murrai@gmail.com>
2023-10-21 10:17:51 -0400

    add flatten.js with function to flatten 2 level arrays into a single level array

---
+++ flatten.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    let out = true;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        out = false;
+      }
+    }
+    return out;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} === ${array2}`);
+  }
+};
+
+
+const flatten = function(source) {
+  let flat = [];
+  for (const item of source) {
+    if (Array.isArray(item)) {
+      for (const elem of item) {
+        flat.push(elem);
+      }
+    } else {
+      flat.push(item);
+    }
+  }
+  return flat;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([]),[]);
