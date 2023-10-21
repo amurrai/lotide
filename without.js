a987b3184fe51d939a144845567ec6519d77acfb
amurrai <andre.murrai@gmail.com>
2023-10-21 09:32:02 -0400

    add without.js with function to remove unwanted items from an array

---
+++ without.js
@@ -0,0 +1,44 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    let out = true;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        out = false;
+      }
+    }
+    return out;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("✅✅✅ Assertion passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion failed");
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let trim = [];
+  for (const item of source) {
+    let unique = true;
+    for (const compare of itemsToRemove) {
+      if (item === compare) {
+        unique = false;
+      }
+    }
+    if (unique === true) {
+      trim.push(item);
+    }
+  }
+  return trim;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
