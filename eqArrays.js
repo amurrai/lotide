1b2ca9a29d823a653f183df034826e0d3b952efb
amurrai <andre.murrai@gmail.com>
2023-10-25 21:43:36 -0400

    Refactor eqArrays to remove unnecessary variables

--- eqArrays.js
+++ eqArrays.js
@@ -10,13 +10,12 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } else {
-    let out = true;
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
         return false;
       }
     }
-    return out;
+    return true;
   }
 };
 
