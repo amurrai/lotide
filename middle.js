1e73bc4a2e8ac110d05a71becf95722c5a9709ac
amurrai <andre.murrai@gmail.com>
2023-11-09 23:39:50 -0500

    refactor middle to reduce repetition

--- middle.js
+++ middle.js
@@ -1,11 +1,13 @@
 const middle = function(array) {
   let mid = [];
+  let midIndex = Math.floor(array.length / 2);
+  
   if (array.length > 2) {
     if (array.length % 2 === 0) {
-      mid.push(array[Math.floor(array.length / 2) - 1]);
-      mid.push(array[Math.floor(array.length / 2)]);
+      mid.push(array[midIndex - 1]);
+      mid.push(array[midIndex]);
     } else {
-      mid.push(array[Math.floor(array.length / 2)]);
+      mid.push(array[midIndex]);
     }
   }
   return mid;
