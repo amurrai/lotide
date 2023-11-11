d583d3876edb4ab7ad734434720d76f8c1a9cbd7
amurrai <andre.murrai@gmail.com>
2023-11-10 23:54:05 -0500

    Refactor eqArrays to allow deep comparison trough recursion

--- eqArrays.js
+++ eqArrays.js
@@ -3,8 +3,12 @@ const eqArrays = function(array1, array2) {
     return false;
   } else {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+        if (!eqArrays(array1[i], array2[i])) return false;
+      } else {
+        if (array1[i] !== array2[i]) {
+          return false;
+        }
       }
     }
     return true;
